c90c801c18722a6632bd7e131a7f232aa2344ea0
roma4747 <ferz13111980@gmail.com>
2024-12-19 21:21:20 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const hamburger = document.getElementById("mob-hamburger");
+const menu = document.getElementById("nav-mob-wrapper");
+const closeBtn = document.getElementById("nav-mob-close-btn");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.add("open");
+  hamburger.classList.add("hidden");
+  closeBtn.classList.add("visible");
+
+  document.body.classList.add("no-scroll");
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("open");
+  hamburger.classList.remove("hidden");
+  closeBtn.classList.remove("visible");
+
+  document.body.classList.remove("no-scroll");
+});
